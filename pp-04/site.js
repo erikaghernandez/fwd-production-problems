97937ff1998af20a0804499c62966e428ea13d3a
Erika Hernandez <erikaghernandez212@gmail.com>
2021-11-15 22:23:22 -0600

    Modify double() function

--- pp-04/site.js
+++ pp-04/site.js
@@ -26,8 +26,17 @@ console.log('The value of x is', x, '-- it should be 5.');
   can accurately test your work.
 */
 
+var x = 5;
 
+var double  = function(num){
 
+  var x = num * 2;
+  return x;
+
+}
+
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
 
 
 /*
